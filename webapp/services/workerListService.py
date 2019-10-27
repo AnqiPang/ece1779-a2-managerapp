14d7efd86bb2e1f7ea0f7ce814d001475614a2e9
ryang <rita.yang@sentaca.com>
2019-10-27 13:18:12 -0400

    fix bug

--- webapp/services/workerListService.py
+++ webapp/services/workerListService.py
@@ -6,25 +6,13 @@ import boto3
 
 
 class WorkListService:
-    # CLIENT = boto3.client('elbv2', aws_access_key_id=current_app.config["AWS_ACCESS_KEY_ID"],
-    #                       aws_secret_access_key=current_app.config["AWS_SECRET_ACCESS_KEY"])
-    #
-    # TARGET_GROUP = CLIENT.describe_target_health(
-    #   TargetGroupArn=current_app.config["TARGET_GROUP_ARN"])
-    #
-    # CLOUD_WATCH = boto3.client("cloudwatch", aws_access_key_id=current_app.config["AWS_ACCESS_KEY_ID"],
-    #                            aws_secret_access_key=current_app.config["AWS_SECRET_ACCESS_KEY"])
-
     CLIENT = boto3.client('elbv2')
-
-    TARGET_GROUP = CLIENT.describe_target_health(
-      TargetGroupArn=current_app.config["TARGET_GROUP_ARN"])
-
     CLOUD_WATCH = boto3.client("cloudwatch")
 
     def get_instances_cpu_matrix_images(self):
+        target_group = self.CLIENT.describe_target_health(TargetGroupArn=current_app.config["TARGET_GROUP_ARN"])
         charts = []
-        for target in self.TARGET_GROUP['TargetHealthDescriptions']:
+        for target in target_group['TargetHealthDescriptions']:
             instance_id = target['Target']['Id']
             CPUUtilization_REQUEST["metrics"][0][3] = instance_id
             response = self.CLOUD_WATCH.get_metric_widget_image(MetricWidget=json.dumps(CPUUtilization_REQUEST))
