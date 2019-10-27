c411b858b202930f04f1fc5c1d571fbc9b9f8626
ryang <rita.yang@sentaca.com>
2019-10-27 17:18:04 -0400

    UI - manually grow and shrink pool size

--- webapp/controllers/workerManagementController.py
+++ webapp/controllers/workerManagementController.py
@@ -1,4 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
 workerManagement = Blueprint('workerManagement', __name__)
+
+
+@workerManagement.route("/workers/management", methods=["GET"])
+def manage_worker():
+    return render_template("workerManagement.html")
