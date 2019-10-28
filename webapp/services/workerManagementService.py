3208544886731198650dd567452124311544eeb3
ryang <rita.yang@sentaca.com>
2019-10-28 01:51:03 -0400

    fix bug

--- webapp/services/workerManagementService.py
+++ webapp/services/workerManagementService.py
@@ -1,19 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, IntegerField, FloatField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import NumberRange, DataRequired
 from webapp.repository import workerManagementRepo
 
 
 class AutoScalingPolicyForm(FlaskForm):
     thresholdForGrowing = IntegerField('CPU threshold for growing the pool',
-                                       validators=[DataRequired(), Length(min=0, max=100)])
+                                       validators=[NumberRange(min=0, max=100)])
     thresholdForShrinking = IntegerField('CPU threshold for shrinking the pool',
-                                       validators=[DataRequired(), Length(min=0, max=100)])
-    ratioToGrowing = FloatField('Ratio to expand the pool',
-                                       validators=[DataRequired(), Length(min=0, max=10)])
-    ratioToShrinking = FloatField('Ratio to shrink the pool',
-                                       validators=[DataRequired(), Length(min=0, max=10)])
+                                       validators=[NumberRange(min=0, max=100)])
+    ratioToGrowing = FloatField('Ratio to expand the pool', validators=[NumberRange(min=0, max=10)])
+    ratioToShrinking = FloatField('Ratio to shrink the pool', validators=[NumberRange(min=0, max=10)])
     submit = SubmitField('Submit')
+
 
 class WorkerManagementService:
     def save_management_data(self, threshold_growing, threshold_shrinking, ratio_growing, ratio_shrinking):
