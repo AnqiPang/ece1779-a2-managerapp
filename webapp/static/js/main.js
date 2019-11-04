6391d9fa2eb7be5e4cdd25a1023da991a450fd6c
dukoo <14yc26@queensu.ca>
2019-11-04 18:10:33 -0500

    added main.js

--- webapp/static/js/main.js
+++ webapp/static/js/main.js
@@ -1,1 +1,12 @@
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+$(document).ready(function() {
+    $("#growlink").click(function() {
+      // disable button
+      $(this).prop("hidden", true);
+      $("#growlink1").prop("hidden", false)
+    });
+});
